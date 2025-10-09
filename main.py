7fe34e73394c67f94c92fd7b2451ba33cd3a2a10
YiorzHe <amirhafizi443@gmail.com>
2025-10-09 20:10:14 +0800

    newly updated Flask backend

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 # main.py
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from fastapi.requests import Request
+from flask import Flask, render_template
 
-app = FastAPI()
+app = Flask(__name__)
 
-# Serve static files (like app.js)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-templates = Jinja2Templates(directory="templates")
-
-@app.get("/")
-async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+if __name__ == "__main__":
+    app.run(debug=True)
